6f9f46900d3653d60e1fef42c9846424091a658b
André Kurth <mail@andre-kurth.de>
2022-12-20 07:59:46 +0100

    Adding required files

---
+++ module/warlock-skin.mjs
@@ -0,0 +1,37 @@
+Hooks.once('init', () => {
+    CONFIG.canvasTextStyle = foundry.utils.mergeObject(
+        CONFIG.canvasTextStyle,
+        {
+            "styleID": 41,
+            "_align": "center",
+            "_breakWords": false,
+            "_dropShadow": false,
+            "_dropShadowAlpha": 1,
+            "_dropShadowAngle": 0,
+            "_dropShadowBlur": 2,
+            "_dropShadowColor": "#000000",
+            "_dropShadowDistance": 0,
+            "_fill": "#111111",
+            "_fillGradientType": 0,
+            "_fillGradientStops": [],
+            "_fontFamily": "Courier New",
+            "_fontSize": 36,
+            "_fontStyle": "normal",
+            "_fontVariant": "normal",
+            "_fontWeight": "bold",
+            "_letterSpacing": 0,
+            "_lineHeight": 0,
+            "_lineJoin": "miter",
+            "_miterLimit": 10,
+            "_padding": 1,
+            "_stroke": "#111111",
+            "_strokeThickness": 0,
+            "_textBaseline": "alphabetic",
+            "_trim": false,
+            "_whiteSpace": "pre",
+            "_wordWrap": false,
+            "_wordWrapWidth": 100,
+            "_leading": 0
+        }
+    ) ;
+});
